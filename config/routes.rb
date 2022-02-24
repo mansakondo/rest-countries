bee2bc2c383433d4abd4214ed2939cd066606403
mansakondo <mansakondo22@gmail.com>
2022-02-24 17:08:40 +0100

    Redesign the app with progressive enhancement

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'countries#index'
+  root 'countries#index', as: 'countries'
   get ':name/', to: 'countries#show', as: 'country'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
