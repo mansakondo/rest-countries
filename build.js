446a68725ac749332b8186b01ad2b53ef06ea4b8
mansakondo <mansakondo22@gmail.com>
2022-03-01 15:24:46 +0100

    Update esbuild script

--- build.js
+++ build.js
@@ -3,6 +3,14 @@
 const path  = require("path")
 const rails = require("esbuild-rails")
 
+const watchMode = process.argv.includes("--watch")
+const onRebuild = {
+  onRebuild(error, result) {
+    if (error) console.error('watch build failed:', error)
+    else console.log('watch build succeeded:', result)
+  }
+}
+
 require("esbuild").build({
   entryPoints: ["application.js"],
   outdir: path.join(process.cwd(), "app/assets/builds/"),
@@ -11,7 +19,9 @@ require("esbuild").build({
   plugins: [
     rails(),
   ],
-  watch: process.argv.includes("--watch"),
-}).catch(() => process.exit(1))
-
-
+  watch: watchMode ? onRebuild : false,
+}).then(() => {
+  console.log("Watching...")
+}).catch(() => {
+  process.exit(1)
+})
